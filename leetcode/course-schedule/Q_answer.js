e105becb3f2b664af4f8b6861a65d4a4a13f14fb
SEONKYU\difke <somedaycode@gmail.com>
2021-04-21 23:22:31 +0900

    [ Q ]: Update course-schedule answer with using queue

--- leetcode/course-schedule/Q_answer.js
+++ leetcode/course-schedule/Q_answer.js
@@ -1,3 +1,4 @@
+// 92%
 var canFinish = function (numCourses, prerequisites) {
   const linked = Array.from({ length: numCourses }, () => []);
   const inDegree = Array.from({ length: numCourses }).fill(0);
@@ -27,4 +28,33 @@ var canFinish = function (numCourses, prerequisites) {
 
   if (result.length === numCourses) return true;
   else return false;
+};
+
+// 14~20%
+// queue를 pop해서 사용
+// 읽기는 이게 더 좋지만 속도는 훨씬 느리다.
+var canFinish = function (numCourses, prerequisites) {
+  const inDegree = Array.from({ length: numCourses }).fill(0);
+  const result = [];
+  let q = [];
+
+  for (let [course, pre] of prerequisites) {
+    inDegree[course]++;
+  }
+
+  for (let i = 0; i < numCourses; i++) {
+    if (inDegree[i] === 0) q.push(i);
+  }
+
+  while (q.length > 0) {
+    const poped = q.pop();
+    result.push(poped);
+    for (const [course, pre] of prerequisites) {
+      if (pre === poped) {
+        inDegree[course]--;
+        if (inDegree[course] === 0) q.push(course);
+      }
+    }
+  }
+  return result.length === numCourses;
 };
