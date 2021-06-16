023771cfb088e14adcafda06b72ad862093dd976
somedaycode <somedaycode@gmail.com>
2021-06-17 04:35:02 +0900

    [ Q ] refactor answer

--- codility/PermMissingElem/Q_answer.js
+++ codility/PermMissingElem/Q_answer.js
@@ -1,3 +1,15 @@
+// 리팩토링
+function solution(A) {
+  if (A.length === 0) return 1;
+  const toBeSum = Array.from({ length: A.length + 1 }, (_, i) => i + 1).reduce(
+    (prev, curr) => prev + curr,
+    0
+  );
+  const currentSum = A.reduce((prev, curr) => prev + curr, 0);
+  return toBeSum - currentSum;
+}
+
+// 처음
 function solution(A) {
   const sorted = A.sort((a, b) => a - b);
   if (sorted[0] !== 1) return 1;
